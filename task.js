2845921f7175da525d636192d3de6c61d1c1f5ee
yfg1995 <andonovic995@gmail.com>
2024-08-01 00:04:47 +0200

    .

--- task.js
+++ task.js
@@ -39,53 +39,53 @@ export class Task {
     this.wrap.innerHTML = "";
     this.task = createHTMLElement("li", { className: "task" });
 
-    this.taskText = createHTMLElement("span", {
-      textContent: this.inputNewValue,
-    });
-
-    this.taskEditInput = createHTMLElement("input", {
-      className: "task-edit-input",
-      id: "edit-input",
-      value: this.inputNewValue,
-      autofocus: true,
-    });
-
     this.taskButtonsCon = createHTMLElement("div", {
       className: "task-buttons-con",
     });
 
-    this.editButton = createHTMLElement("button", {
-      className: "edit-buttons",
-      textContent: "Edit",
-    });
-
-    this.deleteButton = createHTMLElement("button", {
-      className: "delete-button",
-      textContent: "X",
-    });
-
-    this.cancelButton = createHTMLElement("button", {
-      className: "edit-buttons",
-      textContent: "Cancel",
-    });
-
-    this.saveButton = createHTMLElement("button", {
-      className: "edit-buttons",
-      textContent: "Save",
-    });
-
     if (this.isEditActive) {
+      this.taskEditInput = createHTMLElement("input", {
+        className: "task-edit-input",
+        id: "edit-input",
+        value: this.inputNewValue,
+        autofocus: true,
+      });
+
+      this.cancelButton = createHTMLElement("button", {
+        className: "edit-buttons",
+        textContent: "Cancel",
+      });
+
+      this.saveButton = createHTMLElement("button", {
+        className: "edit-buttons",
+        textContent: "Save",
+      });
+
       this.taskButtonsCon.append(this.cancelButton, this.saveButton);
       this.task.append(this.taskEditInput, this.taskButtonsCon);
     } else {
+      this.taskText = createHTMLElement("span", {
+        textContent: this.inputNewValue,
+      });
+
+      this.editButton = createHTMLElement("button", {
+        className: "edit-buttons",
+        textContent: "Edit",
+      });
+
+      this.deleteButton = createHTMLElement("button", {
+        className: "delete-button",
+        textContent: "X",
+      });
+
       this.taskButtonsCon.append(this.editButton, this.deleteButton);
       this.task.append(this.taskText, this.taskButtonsCon);
     }
 
+    this.wrap.appendChild(this.task);
+
     this.editButton.addEventListener("click", () => this.toggleEdit());
     this.cancelButton.addEventListener("click", () => this.toggleEdit());
-
-    this.wrap.appendChild(this.task);
   }
 
   init() {
